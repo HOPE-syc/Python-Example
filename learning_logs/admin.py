9bb73b3f86f43b608519ff3a98cd81c2c850bcc3
HOPE-syc <yc4332@naver.com>
2023-03-26 21:56:26 +0900

    this is python practice

---
+++ learning_logs/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Topic, Entry
+
+# Register your models here./ 모델을 여기 등록하십시오.
+admin.site.register(Topic)
+admin.site.register(Entry)
---
+++ learning_logs/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here./모델을 여기 만듭니다.
+class Topic(models.Model):
+	"""사용자가 배우고 있는 주제"""
+	text = models.CharField(max_length = 200)
+	date_added = models.DateTimeField(auto_now_add = True)
+	owner = models.ForeignKey(User, on_delete=models.CASCADE)
+	
+	def __str__(self):
+		"""모델을 문자열로 반환합니다"""
+		return self.text
+
+class Entry(models.Model):
+	"""주제에 관해 배운 것"""
+	topic = models.ForeignKey(Topic, on_delete = models.CASCADE)
+	text = models.TextField()
+	date_added = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		verbose_name_plural = 'entries'
+
+		def __str__(self):
+			"""모델을 문자열로 반환합니다"""
+			return f"{self.text[:50]}..."
+
+
